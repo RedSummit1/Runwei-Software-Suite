555e96ef89fac32e6cff9897cafae22f057f81a0
Jonathan Shields <jshieldsf@gmail.com>
2024-02-19 00:15:09 -0500

    Make class more concise and got .csv headers

--- RunweiRSSparser.py
+++ RunweiRSSparser.py
@@ -4,30 +4,54 @@ import feedparser
 
 class FeedParserPro:
 
-    def __init__(self,*args,csv_file,**kwargs):
-        print("Parser online")
-#        try:
-#            with open(csv_file,'a') as file:
-#                self.csv_file = file
-#        except:
-#            pass            
+    def __init__(self):
+        print("Parser online") 
+
+        try:
+            with open("test.csv",'r') as file:
+                self.csv_headers = csv.DictReader(file) 
+                self.csv_headers = next(self.csv_headers).keys() #Get the headers from the file object
+        except:
+            pass            
 
     def fetch(self,xmlFeed):
-        self.RSS = feedparser.parse(xmlFeed)
-        while(not self.RSS["feed"]):
+        self.RSS = feedparser.parse(xmlFeed) #Fetch RSS feed
+        while(not self.RSS["feed"]): # If it is not there ...
+
             try:
-                if not self.RSS["feed"]:
-                    raise SyntaxError (f"{self.RSS['bozo_exception']}\n\tPlease input a valid url.")
-            except SyntaxError as err:
+                raise ValueError ("\n--> No feed received.\n--> Please enter another RSS feed URL.\n")
+            except ValueError as err:
                 print(err)
-                self.RSS = feedparser.parse(input())
-        print("Response Aquired")
+
+            try:
+                self.RSS = feedparser.parse(input("URL: ")) # Anothor attempt to send a request
+            except KeyboardInterrupt:
+                break
+
+        # <!-- TODO I AM ASSUMING THAT THIS IS A VALID RSS URL, NEED TO PUT ADDITIONAL CHECKS LATER
+
+    
 
 
-        
 
-
-
+#
+#
+#
+#
+#        while(not self.RSS["feed"]):
+#            try:
+#                if not self.RSS["feed"]:
+#                    raise SyntaxError (f"{self.RSS['bozo_exception']}\n\tPlease input a valid url.")
+#            except SyntaxError as err:
+#                print(err)
+#                self.RSS = feedparser.parse(input())
+#        print("Response Aquired")
+#
+#    def 
+#        
+#
+#
+#
 
 
 
