baf8abd76a02be41608af63e81d6dd1f76892a33
Jonathan Shields <jshieldsf@gmail.com>
2024-03-12 19:47:16 -0400

    Checkpoint current state of code where values are written to the spreadsheet

--- GoogleSheet.py
+++ GoogleSheet.py
@@ -1,6 +1,7 @@
 from AuthenticateGoogleSheet import authenticate
 from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
+from RunweiRSSparser import FeedParserPro
 
 def create():
 
@@ -19,7 +20,6 @@ def create():
 def writeValues(spreadSheetId:str):
     try:
         service = build("sheets","v4",credentials=authenticate())        
-
         result = (
             service.spreadsheets()
             .values()
@@ -27,7 +27,70 @@ def writeValues(spreadSheetId:str):
             .execute()
         ) 
         rows = result.get("values",[])
-        return rows
+
+        values = [] # Values to be written
+
+#               Code that needs to be reviewed
+#<------------------------------------------------------------------------>
+#        parser = FeedParserPro()
+#        parser.fetch(rows[0][0])
+#        print(parser.csv_headers)
+#        csvheaders = {}.fromkeys(parser.csv_headers,[])
+#        #values = [values[0].append(header) for header in csvheaders]
+#        for row in rows:
+#            parser.csv_headers = None
+#            #breakpoint()
+#            parser.fetch(rows[0][0])
+#            print(parser.RSS.entries[0])
+#            for k,v in parser.RSS.entries[0].items():
+#                csvheaders[k].append("Testing") 
+#
+#        print(len(csvheaders.keys()) == len(csvheaders.values()))
+#        #print(csvheaders.values())
+#        values.extend([[*csvheaders.keys()]])
+#        #print(len(csvheaders.values()))
+#        for v in csvheaders.values():
+#            values.append(v)
+#        values = list(map(lambda x: x[:8],values))
+#        print(len(values))
+#
+        #print(values[0])
+        #values.extend(v for v in csvheaders.values())
+#,[csvheaders.values()])
+
+#<------------------------------------------------------------------------>
+
+        for _ in range(5):
+            values.append(list(range(0,10,2)))
+
+        body = {"values":values}
+        print(len(body["values"]))
+        result = (
+            service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId = spreadSheetId,
+                range=f"B1:F5",
+                valueInputOption="USER_ENTERED",
+                body=body,
+            )
+            .execute()
+        )
+        return type(result)
+
+        #parser = FeedParserPro()
+        #for i,valu
+
+
+
+
+
+
+
+
+
+
+
     except:
         pass
         
